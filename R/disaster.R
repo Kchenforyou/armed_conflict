05bb1ebccb00ac4f9f601424663f231bb7628224
Kevin Chen <Kchenforyou@Gmail.com>
2024-09-16 12:50:59 -0400

    Added comments

--- R/disaster.R
+++ R/disaster.R
@@ -6,19 +6,29 @@ library("readr")
 # import data
 dis <- read_csv("original/disaster.csv") 
 
+# change col name
 colnames(dis)[7] <- "Disaster.type"
 
+# filter year and disaster type
 dis_sub <- filter(dis, Year %in% c(2000:2019), 
                   Disaster.type == "Earthquake" |
                   Disaster.type == "Drought"
-                  ) %>%
-           select(c("Year", "ISO", "Disaster.type"))
+                  ) 
 
+# subset for year,country, and disaster type
+dis_sub <- select(dis_sub, c("Year", "ISO", "Disaster.type"))
+           
+# create dummy variables
 dis_sub$earthquake <- ifelse(dis_sub$Disaster.type == "Earthquake", 1, 0)
 dis_sub$drought <- ifelse(dis_sub$Disaster.type == "Drought", 1, 0)
 
-dis_sub<- dis_sub %>% select(-c("Disaster.type")) %>% 
-  group_by(Year, ISO) %>% 
-  summarize(earthquake = max(earthquake), 
+# remove disaster.type column
+dis_sub <- select(dis_sub, -c("Disaster.type")) 
+
+# grouped dataframe by year and ISO (country)
+dis_sub <- group_by(dis_sub, Year, ISO) 
+
+# used summarize to remove duplicate countries in each year
+dis_sub <- summarize(dis_sub, earthquake = max(earthquake), 
             drought = max(drought))
 
