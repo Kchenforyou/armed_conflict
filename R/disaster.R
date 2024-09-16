8add55fa934f7b32a45f9657c95317c826a58b29
Kevin Chen <Kchenforyou@Gmail.com>
2024-09-16 12:08:00 -0400

    Created R code for Disaster.R

--- R/disaster.R
+++ R/disaster.R
@@ -4,7 +4,17 @@ library("dplyr")
 library("readr")
 
 # import data
-dis <- read_csv("original/disaster.csv")
+dis <- read_csv("original/disaster.csv") 
 
-dis_sub <- filter(dis, )
+colnames(dis)[7] <- "Disaster.type"
 
+dis_sub <- filter(dis, Year %in% c(2000:2019), 
+                  Disaster.type == "Earthquake" |
+                  Disaster.type == "Drought"
+                  ) %>%
+           select(c("Year", "ISO", "Disaster.type"))
+
+dis_sub$earthquake <- ifelse(dis_sub$Disaster.type == "Earthquake", 1, 0)
+dis_sub$drought <- ifelse(dis_sub$Disaster.type == "Drought", 1, 0)
+
+dis_sub %>% group_by(c("ISO")) %>% summarize()
