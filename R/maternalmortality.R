3619ca223dd143628dee221c032af1409d874bed
Kevin Chen <Kchenforyou@Gmail.com>
2024-09-16 11:05:15 -0400

    Changed subsetting for pivot_longer function.

--- R/maternalmortality.R
+++ R/maternalmortality.R
@@ -6,7 +6,12 @@ library("readr")
 # import data
 mm <- read_csv("original/maternalmortality.csv")
 
+# pivot table
 mm_sub <- select(mm, c("Country Name", "2000":"2019")) %>% 
-  pivot_longer(cols = 2:21) 
+  pivot_longer(cols = "2000":"2019")
 
+# change col names
 colnames(mm_sub) <- c("Country.name", "year", "MatMor")
+
+# force year as integers
+mm_sub$year <- as.numeric(mm_sub$year)
