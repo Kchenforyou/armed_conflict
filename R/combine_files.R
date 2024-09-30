ad12a6603f5ee83115763f4465fed86268f376ed
Kevin Chen <Kchenforyou@Gmail.com>
2024-09-30 11:15:58 -0400

    Made changes to conflict.R and combine_files.R. Final_data has correct dimensions.

--- R/combine_files.R
+++ R/combine_files.R
@@ -13,9 +13,22 @@ source("R/conflict.R") # returns new_conflictdata
 
 colnames(dis_sub)[1] <- "year"
 
-comb_list <- list(full_data, dis_sub, new_conflictdata, covariates)
+# comb_list <- list(full_data, dis_sub, new_conflictdata, covariates)
+comb_list <- list(new_conflictdata, full_data, dis_sub)
+# for reference: list(conflict data, mortality, disaster)
 keys <- c("year", "ISO")
 
 # Join multiple data.frames
-df2 <- comb_list %>% reduce(inner_join, by = keys)
-df2
+df2 <- comb_list %>% reduce(full_join, by = keys)
+
+# left join with covariates
+final_data <- covariates %>% left_join(df2, by = keys)
+
+# replace NA's
+final_data <- final_data |>
+  mutate(armconf1 = replace_na(armconf1, 0),
+         drought = replace_na(drought, 0),
+         earthquake = replace_na(earthquake, 0),
+         totdeath = replace_na(totdeath, 0))
+
+write.csv(df2,"original/armed_conflict.csv")
--- R/conflict.R
+++ R/conflict.R
@@ -8,11 +8,14 @@ conflictdata <- read_csv("original/conflictdata.csv")
 conflictdata$best <- ifelse(conflictdata$best > 0, 1, 0)
 
 # used summarize to remove duplicate countries in each year
-new_conflictdata <- conflictdata %>% group_by(ISO, year) %>%
-  summarize(best = max(best))
+new_conflictdata <- conflictdata %>% 
+  group_by(ISO, year) %>%
+  summarize(totdeath = max(best)) %>%
+  mutate(armconf1 = ifelse(totdeath > 0, 1, 0)) %>%
+  ungroup() 
 
 # masked 1's and 0's as bools
-new_conflictdata$best <-ifelse(new_conflictdata$best == 1, TRUE, FALSE) 
+# new_conflictdata$best <-ifelse(new_conflictdata$totdeath == 1, TRUE, FALSE) 
 
 # lag year by one
 new_conflictdata$year <- new_conflictdata$year + 1
